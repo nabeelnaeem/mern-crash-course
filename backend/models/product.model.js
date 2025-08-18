757f6943e5ff748a605bda982ca81dfd9adb35ec
Nabeel Naeem <nnabeel03@gmail.com>
2025-08-18 19:29:00 +0500

    feat(product-module): Added and updated product routes, controllers

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -22,6 +22,22 @@ const productSchema = new mongoose.Schema({
     timestamps: true, //createAt, updatedAt
 });
 
+//This will include time stamps according to user machine as well
+productSchema.set("toJSON", {
+    transform: (doc, ret) => {
+        if (ret.createdAt) {
+            ret.createdAtLocal = new Date(ret.createdAt).toLocaleString();
+        }
+        if (ret.updatedAt) {
+            ret.updatedAtLocal = new Date(ret.updatedAt).toLocaleString();
+        }
+        if (ret.deletedAt) {
+            ret.deletedAtLocal = ret.deletedAt ? new Date(ret.deletedAt).toLocaleString() : null;
+        }
+        return ret;
+    }
+});
+
 //Now we have schema defined above, we can create model from that schema
 const Product = mongoose.model('Product', productSchema);
 //Here we have to put the singular 'Product' and mongoose will convert it into collection products
